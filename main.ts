b6cf37afc0113d596ba902d6ae8cdb68928b5ac7
LStan23 <161054216+LStan23@users.noreply.github.com>
2024-02-23 15:54:45 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -79,10 +79,10 @@ let mySprite = sprites.create(img`
 animation.runMovementAnimation(
 mySprite,
 animation.animationPresets(animation.flyToCenter),
-2000,
+500,
 true
 )
-controller.moveSprite(mySprite)
+controller.moveSprite(mySprite, 100, 100)
 scene.setBackgroundColor(10)
 scene.setBackgroundImage(img`
     7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
@@ -206,27 +206,4 @@ scene.setBackgroundImage(img`
     7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
     7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
     `)
-let mySprite2 = sprites.create(img`
-    . . . . . . . . . . b 5 b . . . 
-    . . . . . . . . . b 5 b . . . . 
-    . . . . . . b b b b b b . . . . 
-    . . . . . b b 5 5 5 5 5 b . . . 
-    . . . . b b 5 d 1 f 5 d 4 c . . 
-    . . . . b 5 5 1 f f d d 4 4 4 b 
-    . . . . b 5 5 d f b 4 4 4 4 b . 
-    . . . b d 5 5 5 5 4 4 4 4 b . . 
-    . . b d d 5 5 5 5 5 5 5 5 b . . 
-    . b d d d d 5 5 5 5 5 5 5 5 b . 
-    b d d d b b b 5 5 5 5 5 5 5 b . 
-    c d d b 5 5 d c 5 5 5 5 5 5 b . 
-    c b b d 5 d c d 5 5 5 5 5 5 b . 
-    . b 5 5 b c d d 5 5 5 5 5 d b . 
-    b b c c c d d d d 5 5 5 b b . . 
-    . . . c c c c c c c c b b . . . 
-    `, SpriteKind.Enemy)
-animation.runMovementAnimation(
-mySprite2,
-animation.animationPresets(animation.bobbingLeft),
-2000,
-true
-)
+effects.bubbles.startScreenEffect()
